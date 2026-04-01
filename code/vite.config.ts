e0105d071ac94dd9d5c40fe38583fcf8823aba7e
bibidevv <abissetou.tounkara@hotmail.com>
2026-04-01 11:17:12 +0200

    intégration sql

--- code/vite.config.ts
+++ code/vite.config.ts
@@ -4,6 +4,10 @@ import rsc from "@vitejs/plugin-rsc";
 import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
+	// modifier l'hôte MySQL pour github actions
+	if (process.env.GITHUB_ACTIONS) {
+		process.env.MYSQL_HOST = "127.0.0.1";
+	}
 	return {
 		server: {
 			port: 5173,
